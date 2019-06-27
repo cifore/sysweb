eed8d295f9cfa1a86ee3e69abbb38cc79c3a71b6
dapengchina <aoqishequ@163.com>
2019-06-27 18:19:57 +0800

    Change-Id: Ie05772f6279d49d40c4e2d6130ed554950b699da

--- src/main/java/com/csi/sbs/sysweb/business/controller/SyswebController.java
+++ src/main/java/com/csi/sbs/sysweb/business/controller/SyswebController.java
@@ -10,63 +10,69 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.annotations.Api;
 import springfox.documentation.annotations.ApiIgnore;
 
-@CrossOrigin//解决跨域请求
+@CrossOrigin // 解决跨域请求
 @Controller
 @RequestMapping("/sysweb")
 @Api(value = "Then controller is sysconfig")
 public class SyswebController {
-       
-       @RequestMapping(value = "/index", method = RequestMethod.GET)
-       @ApiIgnore()
-   	   public String index() throws JsonProcessingException{
-    	  return "index";
-   	   }
 
-       @RequestMapping(value = "/index/testApi", method = RequestMethod.GET)
-       @ApiIgnore()
-   	   public String testApi() throws JsonProcessingException{
-    	  return "testApi";
-   	   }
-   	  
-   	   @RequestMapping(value = "/currency", method = RequestMethod.GET)
-       @ApiIgnore()
-   	   public String currency() throws JsonProcessingException{
-    	  return "currency";
-   	   }
-   	   
-	   @RequestMapping(value = "/merchant", method = RequestMethod.GET)
-	   @ApiIgnore()
-	   public String creditcard() throws JsonProcessingException{
-		   return "merchant";
-	   }
-	   
-	   @RequestMapping(value = "/amountRange", method = RequestMethod.GET)
-	   @ApiIgnore()
-	   public String amountRange() throws JsonProcessingException{
-		   return "amountRange";
-	   }
-	   
-	   @RequestMapping(value = "/tdRate", method = RequestMethod.GET)
-	   @ApiIgnore()
-	   public String tdRate() throws JsonProcessingException{
-		   return "tdRate";
-	   }
-	   
-	   @RequestMapping(value = "/sysConfig", method = RequestMethod.GET)
-	   @ApiIgnore()
-	   public String sysConfig() throws JsonProcessingException{
-		   return "sysconfig";
-	   }
-	   
-	   @RequestMapping(value = "/module", method = RequestMethod.GET)
-	   @ApiIgnore()
-	   public String module() throws JsonProcessingException{
-		   return "module";
-	   }
-	   
-	   @RequestMapping(value = "/holiday", method = RequestMethod.GET)
-	   @ApiIgnore()
-	   public String holiday() throws JsonProcessingException{
-		   return "holiday";
-	   }
+	@RequestMapping(value = "/home")
+	@ApiIgnore()
+	public String home() {
+		return "home";
+	}
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	@ApiIgnore()
+	public String index() throws JsonProcessingException {
+		return "index";
+	}
+
+	@RequestMapping(value = "/index/testApi", method = RequestMethod.GET)
+	@ApiIgnore()
+	public String testApi() throws JsonProcessingException {
+		return "testApi";
+	}
+
+	@RequestMapping(value = "/currency", method = RequestMethod.GET)
+	@ApiIgnore()
+	public String currency() throws JsonProcessingException {
+		return "currency";
+	}
+
+	@RequestMapping(value = "/merchant", method = RequestMethod.GET)
+	@ApiIgnore()
+	public String creditcard() throws JsonProcessingException {
+		return "merchant";
+	}
+
+	@RequestMapping(value = "/amountRange", method = RequestMethod.GET)
+	@ApiIgnore()
+	public String amountRange() throws JsonProcessingException {
+		return "amountRange";
+	}
+
+	@RequestMapping(value = "/tdRate", method = RequestMethod.GET)
+	@ApiIgnore()
+	public String tdRate() throws JsonProcessingException {
+		return "tdRate";
+	}
+
+	@RequestMapping(value = "/sysConfig", method = RequestMethod.GET)
+	@ApiIgnore()
+	public String sysConfig() throws JsonProcessingException {
+		return "sysconfig";
+	}
+
+	@RequestMapping(value = "/module", method = RequestMethod.GET)
+	@ApiIgnore()
+	public String module() throws JsonProcessingException {
+		return "module";
+	}
+
+	@RequestMapping(value = "/holiday", method = RequestMethod.GET)
+	@ApiIgnore()
+	public String holiday() throws JsonProcessingException {
+		return "holiday";
+	}
 }
---
+++ src/main/resources/static/js/main/home.js
@@ -0,0 +1,47 @@
+$(function () {
+	$('iframe').attr('src',''); 
+});
+
+function adminList(){
+	$('iframe').attr('src',''); 
+}
+
+function userList(){
+	$('iframe').attr('src',''); 
+}
+
+function taskList(){
+	$('iframe').attr('src',''); 
+}
+
+function wordList(){
+	$('iframe').attr('src','');
+}
+
+function sysParamList(){
+	$('iframe').attr('src','');
+}
+
+function reportList(){
+	$('iframe').attr('src','');
+}
+
+function thesaureList(){
+	$('iframe').attr('src','');
+}
+
+function taskProgressReport(){
+	$('iframe').attr('src','');
+}
+
+function courseApplyList(){
+	$('iframe').attr('src','');
+}
+
+
+
+
+
+
+
+
